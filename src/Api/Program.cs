7380c44030b04fdbc00290aa17b54bbe3cbae3de
Daniel Cazzulino <daniel@cazzulino.com>
2025-03-01 02:51:48 -0300

    Add distributed cache support

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http;
+using Devlooped;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,9 +17,21 @@ builder.ConfigureFunctionsWebApplication()
 //     .ConfigureFunctionsApplicationInsights();
 
 builder.Services.AddHttpClient();
+#pragma warning disable EXTEXP0018 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+builder.Services.AddHybridCache();
+
+builder.Services.AddDistributedAzureTableStorageCache(options =>
+{
+    options.ConnectionString = builder.Configuration["AzureWebJobsStorage"];
+    options.PartitionKey = "quotes";
+    options.TableName = "cache";
+});
 
 builder.Services.AddSingleton(sp => MarketStackService.Create(
     sp.GetRequiredService<IHttpClientFactory>(),
     sp.GetRequiredService<IConfiguration>()));
+
+builder.Services.AddSingleton(sp => CloudStorageAccount.Parse(
+    sp.GetRequiredService<IConfiguration>()["AzureWebJobsStorage"]));
 
 builder.Build().Run();
